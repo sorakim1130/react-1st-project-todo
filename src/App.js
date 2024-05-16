fd9e4ec3d56721090734e2868bbf9ae784c50996
sorakim <sora6418@gmail.com>
2024-05-16 14:55:35 +0900

    feat: TodoList 기능개발 및 스타일 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
+import { useState } from "react";
 import "./App.css";
+import "./reset.css";
+import styled from "@emotion/styled";
+import TodoForm from "./components/TodoForm";
+import TodoListBlock from "./components/TodoListBlock";
 
 function App() {
-  return <div>TODO</div>;
+  const initialState = [];
+  const [todos, setTodos] = useState(initialState);
+
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodoStatus = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
+      ),
+    );
+  };
+
+  const removeUser = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <Container>
+      <h1>TODO LIST</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoListBlock
+        todos={todos}
+        toggleTodoStatus={toggleTodoStatus}
+        removeUser={removeUser}
+        isDone={false}
+      />
+      <TodoListBlock
+        todos={todos}
+        toggleTodoStatus={toggleTodoStatus}
+        removeUser={removeUser}
+        isDone={true}
+      />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 1200px;
+  height: 800px;
+  margin: 0 auto;
+  overflow-y: auto;
+  border: 2px solid black;
+`;
 
 export default App;
